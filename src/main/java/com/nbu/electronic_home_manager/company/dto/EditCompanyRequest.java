9774433b3ba18276b1d4dabe29bf29ee870be55c
Momchil Tsanov <momchil395@gmail.com>
2025-12-15 20:07:07 +0200

    Adds company registration functionality
    Implements the initial company service with registration and editing capabilities.
    Sets up data transfer objects for company registration and modification requests.
    Defines a basic service layer to manage company data persistence.

---
+++ src/main/java/com/nbu/electronic_home_manager/company/dto/EditCompanyRequest.java
@@ -0,0 +1,11 @@
+package com.nbu.electronic_home_manager.company.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditCompanyRequest {
+}
---
+++ src/main/java/com/nbu/electronic_home_manager/company/service/CompanyService.java
@@ -0,0 +1,46 @@
+package com.nbu.electronic_home_manager.company.service;
+
+import com.nbu.electronic_home_manager.company.dto.EditCompanyRequest;
+import com.nbu.electronic_home_manager.company.dto.RegisterCompanyRequest;
+import com.nbu.electronic_home_manager.company.model.Company;
+import com.nbu.electronic_home_manager.company.repository.CompanyRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CompanyService {
+
+
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+
+    public void createCompany(RegisterCompanyRequest request) {
+        Company company;
+
+
+        companyRepository.save(company);
+    }
+
+
+    public void editCompany(EditCompanyRequest request) {
+        Company company;
+
+
+        companyRepository.save(company);
+
+    }
+
+    public void deleteCompany() {
+        Company company;
+
+
+        companyRepository.save();
+    }
+
+
+}
